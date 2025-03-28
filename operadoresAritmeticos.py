f5f1906f31aa95e8b5972840908851200a9cf4e8
ISMAEL FELIX MONTEIRO DA CUNHA <202404486028@alunos.unifanor.edu.br>
2025-03-28 10:03:14 -0300

    1

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,27 @@
+# Adição 
+numero1 = 10
+numero2 = 20
+
+soma = numero1 +numero2
+print (soma)
+
+#Subtraçao
+
+subtracao = numero1 - numero2
+print(subtracao)
+
+# Multiplicação
+multiplicacao = numero1 * numero2 
+print(multiplicacao)
+# Divisao
+divisao = numero1 / numero2
+print(divisao)
+# Potencia
+potencia = numero1 ** numero2
+print (potencia)
+# Modulo
+modulo = numero1 % numero2
+print (modulo)
+# Resto da divisao
+restodedivisao = numero1 // numero2
+print (restodedivisao)
