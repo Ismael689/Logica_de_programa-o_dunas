46219525a00de3f4a4af32a05f281ba53396d550
ISMAEL FELIX MONTEIRO DA CUNHA <202404486028@alunos.unifanor.edu.br>
2025-03-28 10:57:02 -0300

    28/03/2025

---
+++ operadoresComparacao.py
@@ -0,0 +1,18 @@
+
+x = 10 
+y = 10 
+
+# Coparacao de igualdade
+print(x == y)
+
+# diferença
+print( x != y )
+
+# Superioriadade (>)
+print(x > y)
+# Inferioridade (<)
+print(x < y)
+# Maior ou igual (>=)
+print(x >= y)
+# Menor ou igual (<=)
+print(x <= y)
